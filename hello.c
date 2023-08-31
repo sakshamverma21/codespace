96fdc518200e028bea87051420dcf78f0f19f458
sakshamverma21 <143409048+sakshamverma21@users.noreply.github.com>
2023-08-31 06:36:02 +0000

    Thu, Aug 31, 2023, 6:36 AM +00:00

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
-    string answer= get_string ("what's your name? ");
-    printf ("hello,%s\n");
+    string first= get_string ("what's your first name? ");
+    string last= get_string ("what's your last name? ");
+    printf ("hello, %s %s\n", first, last);
 }
